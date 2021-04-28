9653286e7fcf734e2a8d32562292c84ee56c9e3b
Şaban Kayış <sbnkys1@gmail.com>
2021-04-29 01:22:12 +0300

    working with multiple component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import './App.css';
+import GoalList from "./components/GoalList";
+import "./App.css";
 
 // in the modern react use the functional component
-const App = () => <h1 title="this is title"><span>This</span> is React</h1>
+const App = () => {
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <GoalList />
+    </div>
+  );
+};
 
 // class App extends React.Component {
 //   render(){
---
+++ src/components/GoalList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./GoalList.css";
+
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Finish the course</li>
+      <li>Learn all about the react</li>
+      <li>Help other students in the course</li>
+    </ul>
+  );
+};
+
+export default GoalList;
