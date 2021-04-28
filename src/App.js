c8687e9679a613a53e7c6fa6af483255642d4b4f
Şaban Kayış <sbnkys1@gmail.com>
2021-04-29 02:38:45 +0300

    managing state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import GoalList from "./components/GoalList/GoalList";
 import NewGoal from "./components/NewGoal/NewGoal";
 import "./App.css";
 
 // in the modern react use the functional component
 const App = () => {
-  const courseGoals = [
-    {id:"cg1",text:"Finish the Tutorial"},
-    {id:"cg2",text:"Learn all about the react"},
-    {id:"cg3",text:"Help other students in the course"},
-  ];
-const addNewGoalHandler = (NewGoal) =>{
-  courseGoals.push(NewGoal);
-  console.log(courseGoals);
-}
+  const [courseGoals,setCourseGoals] = useState( [
+    { id: "cg1", text: "Finish the Tutorial" },
+    { id: "cg2", text: "Learn all about the react" },
+    { id: "cg3", text: "Help other students in the course" },
+  ]);
+  const addNewGoalHandler = (NewGoal) => {
+    // setCourseGoals([...courseGoals,NewGoal])
+    //Alternative is concat() methot
+    setCourseGoals(courseGoals.concat(NewGoal));
+  };
   return (
     <div className="course-goals">
-    <NewGoal onAddGoal={addNewGoalHandler} />
+      <NewGoal onAddGoal={addNewGoalHandler} />
       <h2>Course Goals</h2>
       <GoalList goalsData={courseGoals} />
     </div>
