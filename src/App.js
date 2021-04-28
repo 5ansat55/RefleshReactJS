37d88081b281f9cbf3aa5cf206d583cf3358046e
Şaban Kayış <sbnkys1@gmail.com>
2021-04-29 00:47:21 +0300

    understanding react class && functional  components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import './App.css';
 
+// in the modern react use the functional component
 const App = () => <h1 title="this is title"><span>This</span> is React</h1>
 
+// class App extends React.Component {
+//   render(){
+//     return <h1 title="this is title"><span>This</span> is React</h1>
+//   }
+// }
 export default App;
